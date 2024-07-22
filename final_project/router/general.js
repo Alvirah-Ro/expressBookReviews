f287c40f6fd72b330fcedfadb367dd7e452d8558
Alvirah-Ro <bajoraneh@hotmail.com>
2024-07-21 21:26:16 -0400

    still book list

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,7 @@ public_users.post("/register", (req,res) => {
 /// Get the book list available in the shop
 public_users.get('/',function (req, res) {
  //Write your code here
- res.status(200).json(JSON.stringify(books, null, 2));
+ res.status(200).json(books);
 });
 
 
