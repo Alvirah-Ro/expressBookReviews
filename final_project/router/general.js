efe77d3ddf928347d433f8e09aab35b37cf6e47a
Alvirah-Ro <bajoraneh@hotmail.com>
2024-07-23 03:05:48 +0000

    now trying author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,16 @@ public_users.get("/id/:id", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  //const author = req.params.author;
+  const author = decodeURIComponent(req.params.author);
+
+  let filtered_books = books.filter((author) => books.author === author);
+
+  if filtered_books.lemgth > 0 {
+    return res.json(filtered_books);
+  } else {
+    return res.status(404).json({ message: "book not found" });
+  }
 });
 
 // Get all books based on title
