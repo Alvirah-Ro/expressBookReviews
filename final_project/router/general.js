1ec0332b85245d2d9b64d875d6597e29edd1a02d
Alvirah-Ro <bajoraneh@hotmail.com>
2024-07-23 02:41:10 +0000

    there was no isbn, only id

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,10 +16,15 @@ public_users.get("/", function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  let filtered_books = books.filter((books) => books.isbn === isbn);
-  return res.send.json(filtered_books);
+public_users.get("/id/:id", function (req, res) {
+  //const id = req.params.id;
+  const id = parseInt(req.params.id, 10);
+
+  if (books[id]) {
+    return res.json(books[id]);
+  } else {
+    return res.status(404).json({ message: "book not found" });
+  }
 });
 
 // Get book details based on author
