d136557d72426ab9af132579100377a8d7594fe2
Alvirah-Ro <bajoraneh@hotmail.com>
2024-07-26 14:23:18 +0000

    main branch new customer registration working

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username) => { 
-    return !users.some(user => user.username === username);
-}
+const isValid = (username) => {
+  return !users.some((user) => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ 
-    return users.some(user => user.username === username && user.password === password);
-}
-
-regd_users.post("/register", (req, res) => {
-    const {username, password} = req.body;
-
-    if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required"});
-    }
-
-    if (!isValid(username)) {
-        return res.status(409).json({ message: "Username already exists"});
-    }
-
-    users.push({username, password});
-
-    return res.status(201).json({ message: "User registered successfully"});
-});
+const authenticatedUser = (username, password) => {
+  return users.some(
+    (user) => user.username === username && user.password === password,
+  );
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
