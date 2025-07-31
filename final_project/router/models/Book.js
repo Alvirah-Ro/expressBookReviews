472ad44dd51f8927263c109c94cfe097c192d518
Alvirah-Ro <bajoraneh@hotmail.com>
2025-07-31 17:56:52 -0500

    added more detail to review schema

--- final_project/router/models/Book.js
+++ final_project/router/models/Book.js
@@ -4,7 +4,12 @@ const bookSchema = new mongoose.Schema({
   id: String,
   title: String,
   author: String,
-  reviews: Object
+  reviews: [{
+    username: String,
+    text: String,
+    rating: Number,
+    date: { type: Date, default: Date.now }
+  }]
 });
 
 module.exports = mongoose.model('Book', bookSchema);
